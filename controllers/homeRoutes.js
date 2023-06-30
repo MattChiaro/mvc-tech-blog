978fe6f8bfa2dd2f05baac5cbef04331e43d3a76
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-29 23:58:56 -0500

    pass user and comment data into '/' route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,29 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-    });
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['body'],
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'id'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            loggedIn: req.session.loggedIn,
+            posts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
