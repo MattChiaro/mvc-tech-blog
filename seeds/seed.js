ee7eb6f82731111678a3daefe2e0129183734455
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-30 07:25:46 -0500

    simplified

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,24 +8,14 @@ const commentData = require('./commentData.json')
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData,
+    await User.bulkCreate(userData,
         {
             individualHooks: true,
             returning: true,
         });
 
-    for (const post of postData) {
-        await Post.create({
-            ...post,
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-    }
-    for (const comment of commentData) {
-        await Comment.create({
-            ...comment,
-            user_id: users[Math.floor(Math.random() * users.length)].id
-        });
-    }
+    await Post.bulkCreate(postData);
+    await Comment.bulkCreate(commentData);
 
     process.exit(0);
 };
