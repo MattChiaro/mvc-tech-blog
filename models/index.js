42817d2cf0b59a728fd95694a351fbc8e580d447
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-29 21:32:14 -0500

    db relationships completed (i think)

--- models/index.js
+++ models/index.js
@@ -2,3 +2,37 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// User can create many posts - upon deletion of user, delete their posts.
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// User can create many comments - upon deletion of user, delete their comments.
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//Posts can have many comments - upon deletion of post, delete its comments.
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+//Comments belong to a post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+// Posts must have a user
+Post.belongsTo(User, {
+    as: 'poster',
+    foreignKey: 'user_id',
+})
+
+// Comments must have a user
+Comment.belongsTo(User, {
+    as: 'commenter',
+    foreignKey: 'user_id',
+})
