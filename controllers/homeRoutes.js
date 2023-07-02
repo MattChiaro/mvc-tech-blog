dfea7568c960c2c569b08067797b559645c81f4f
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-02 17:33:00 -0500

    dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,4 +69,10 @@ router.get('/login', (req, res) => {
         });
 });
 
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        logged_in: req.session.logged_in,
+    });
+});
+
 module.exports = router;
